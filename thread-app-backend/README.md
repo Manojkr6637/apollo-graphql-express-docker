b98664f0cbe6de97663927694e5110504ceea9cb
Manoj <manojkr6637@gmail.com>
2024-01-25 06:10:52 +0530

    initial

---
+++ thread-app-backend/README.md
@@ -0,0 +1,16 @@
+# Apollo server steup and configuration typscript express and docker
+
+yarn init
+yarn add express
+npx tsc --init
+yarn add typescript -d
+# "outDir": "./build",  
+# "rootDir": "./src",   
+yarn add @types/express -D
+
+yarn add tsc-watch   -D
+
+yarn add @apollo/server
+yarn add @apollo/cleint
+yarn add graphql
+npx gitignore node
---
+++ thread-app-backend/src/index.ts
@@ -0,0 +1,36 @@
+
+import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import {expressMiddleware } from '@apollo/server/express4';
+
+async function init(){   
+  const port = Number(process.env.PORT) || 8000
+  const app = express(); 
+  app.use(express.json());     
+    const server = new ApolloServer({
+      typeDefs: `
+      type Query{
+        hello: String,
+        say(name: String): String
+      }
+      `,
+      resolvers: {
+        Query:{
+          hello:()=>'Hello there, I am an graphql',
+          say:(_, {name}:{name: String})=>`Hello ${name}, I am an graph`
+        }
+      },
+    })
+
+   await server.start();
+     app.use('/graphql', expressMiddleware(server))
+     app.get('/', (req, res) => {
+      res.json({message: 'server is up and running'})
+     })
+    app.listen(port, () => {
+      console.log(`Server listening on ${port}`) 
+    })
+}
+
+init()
+ 
